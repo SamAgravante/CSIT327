7eb10450a7d22b66daf92879ea9c5b418953eb4c
MikhailAg0ny <mikhailjpn@gmail.com>
2024-11-09 12:54:00 +0800

    another update Stonkstracker
    files from static/images_svg some
    changes the table names
    change forums htmls
    
    also the model part

--- StonksTracker/tracker/models.py
+++ StonksTracker/tracker/models.py
@@ -23,12 +23,13 @@ class CustomUserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
-    UserID = models.AutoField(primary_key=True,default=1)
+    UserID = models.AutoField(primary_key=True)
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=50, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
+    last_login = models.DateTimeField(auto_now=True)
 
     objects = CustomUserManager()
 
@@ -36,7 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['username']
 
     class Meta:
-        db_table = "User"
+        db_table = "tracker_user"
 
     def __str__(self):
         return self.username
@@ -47,19 +48,19 @@ class Watchlist(models.Model):
     UserID = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # One-to-one relationship with User model
 
     class Meta:
-        db_table = 'watchlists'  # Use lowercase and plural form for consistency
+        db_table = 'tracker_watchlists'  # Use lowercase and plural form for consistency
 
     def __str__(self):
         return f"Watchlist ID: {self.WatchlistID} for User: {self.UserID.username}"
 
 class PriceHistory(models.Model):
-    historyID = models.AutoField(primary_key=True,default=1)  # Automatically incrementing primary key
-    itemName = models.CharField(max_length=100)  # Item name field
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # Price field
+    historyID = models.AutoField(primary_key=True)  # Automatically incrementing primary key
+    itemName = models.CharField(max_length=100, default="Unknown Item")
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     timestamp = models.DateTimeField(auto_now_add=True)  # Automatically set the field to now when the object is created
 
     class Meta:
-        db_table = "price_history"  # Use lowercase and plural form for consistency
+        db_table = "tracker_pricehistory"  # Use lowercase and plural form for consistency
 
     def __str__(self):
         return f"PriceHistory ID: {self.historyID}, Item: {self.itemName}, Price: {self.price}"
@@ -71,6 +72,6 @@ class Forums(models.Model):
     lastEdited = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Allow null and blank
     class Meta:
-        db_table = 'forums'
+        db_table = 'tracker_forums'
     def __str__(self):
         return self.title
